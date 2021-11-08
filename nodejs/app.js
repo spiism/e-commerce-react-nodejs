5d55502402eedc64dc8ac89ae13e87d453a37f5c
spiismnz <sp.iism@gmail.com>
2021-11-09 01:14:09 +1300

    update mongo db

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,15 +1,25 @@
 const express = require('express');
-const mongoose = require('mongoose');
 require('dotenv').config();
 
 //app
 const app = express();
 
-//db
-mongoose.connect(process.env.DATABASE, {
-    useNewUrlParser: true,
-    useCreateIndex:true,
-}).then(() => console.log('DB connected'))
+// import mongoose
+const mongoose = require('mongoose');
+// load env variables
+const dotenv = require('dotenv');
+dotenv.config()
+ 
+//db connection
+mongoose.connect(
+  process.env.MONGO_URI,
+  {useNewUrlParser: true}
+)
+.then(() => console.log('DB Connected'))
+ 
+mongoose.connection.on('error', err => {
+  console.log(`DB connection error: ${err.message}`)
+});
 
 //routes
 app.get('/', (req, res) => {
@@ -19,5 +29,5 @@ app.get('/', (req, res) => {
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-    console.log('server is running on port ${port}' );
-})
+    console.log(`Server is running on port ${port}`);
+});

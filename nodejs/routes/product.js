613aa940a94d881c220eb4133e91b12df1889dbe
spiismnz <sp.iism@gmail.com>
2021-11-11 00:49:38 +1300

    search product

--- nodejs/routes/product.js
+++ nodejs/routes/product.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { create, productById, read, remove, update, list, listRelated } = require("../controllers/product");
+const { create, productById, read, remove, update, list, listRelated, listCategories, listBySearch } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
@@ -13,6 +13,10 @@ router.put('/product/:productId/:userId', requireSignin, isAuth, isAdmin, update
 
 router.get('/products/', list);
 router.get('/products/related/:productId', listRelated);
+router.get('/products/categories', listCategories);
+
+router.post('/products/by/search', listBySearch);
+
 
 
 

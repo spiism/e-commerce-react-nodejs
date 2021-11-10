028699832570c1a943e2108e8bf0a1b6e28ec608
spiismnz <sp.iism@gmail.com>
2021-11-10 23:09:58 +1300

    product by ID

--- nodejs/controllers/product.js
+++ nodejs/controllers/product.js
@@ -50,3 +50,26 @@ exports.create = (req, res) => {
         });
     });
 };
+
+
+
+
+exports.productById = (req, res, next, id) => {
+    Product.findById(id)
+        .populate('category')
+        .exec((err, product) => {
+            if (err || !product) {
+                return res.status(400).json({
+                    error: 'Product not found'
+                });
+            }
+            req.product = product;
+            next();
+        });
+};
+
+
+exports.read = (req, res) => {
+    req.product.photo = undefined;
+    return res.json(req.product);
+};

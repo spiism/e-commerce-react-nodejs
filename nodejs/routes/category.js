9160d7a194437e0f9d460f84d303f10c55f5ee65
spiismnz <sp.iism@gmail.com>
2021-11-10 13:33:32 +1300

    create category admin role

---
+++ nodejs/routes/category.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const { create} = require('../controllers/category');
+const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
+const { userById } = require('../controllers/user');
+
+
+router.post('/category/create/:userId', requireSignin, isAuth, isAdmin, create);
+
+
+// router.param('categoryId', categoryById);
+router.param('userId', userById);
+
+module.exports = router;

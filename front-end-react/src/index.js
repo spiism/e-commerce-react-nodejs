5116d3341b0dc94f81f6392c29f0e7f923a28b29
spiismnz <sp.iism@gmail.com>
2021-11-12 00:44:35 +1300

    fix err

--- front-end-react/src/index.js
+++ front-end-react/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Routes from './Routes';
+import ReactRoutes from './ReactRoutes';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Routes />
+    <ReactRoutes />
   </React.StrictMode>,
   document.getElementById('root')
 );

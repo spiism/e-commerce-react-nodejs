de641e6675beffc6f750afccb388e36a9acfb3f8
spiismnz <sp.iism@gmail.com>
2021-11-12 01:17:07 +1300

    update

--- front-end-react/src/ReactRoutes.js
+++ front-end-react/src/ReactRoutes.js
@@ -3,9 +3,11 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Signup from './user/Signup';
 import Signin from './user/Signin';
 import Home from './core/Home';
+import Menu from './core/Menu';
 
 const ReactRoutes = () => {
     return (<BrowserRouter>
+        <Menu />
         <Routes>
             <Route path="/" element={ <Home />} />
             <Route path="/Signin" element={ <Signin />} />

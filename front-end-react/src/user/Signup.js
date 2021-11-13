9a8a017f7431e26d76b962801864dea303730485
spiismnz <sp.iism@gmail.com>
2021-11-14 01:08:40 +1300

    user sign up to API

--- front-end-react/src/user/Signup.js
+++ front-end-react/src/user/Signup.js
@@ -11,8 +11,33 @@ const Signup = () => {
         success: false
     });
 
+    const {name, email, password} =values;
+
     const handleChange = name => event => {
         setValues({...values, error: false, [name]:event.target.value})
+    }
+
+    const signup = (user) => {
+        //console.log(name, email, password);
+        fetch(`${API}/signup`, { 
+            method: "POST",
+            headers: {
+                Accept: 'application/json',
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(user)
+        })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => {
+            console.error(err);
+        })
+    }
+
+    const clickSubmit = (event) => {
+        event.preventDefault();
+        signup ({name, email, password});
     }
 
     const signUpForm = () => (
@@ -31,7 +56,7 @@ const Signup = () => {
                 <label className="text-muted">Password</label>
                 <input onChange= {handleChange('password')} type="password" className="form-control" />
             </div>
-            <button className ="btn btn-primary">Submit</button>
+            <button onClick ={clickSubmit} className ="btn btn-primary">Submit</button>
         </form>
     )
 

f54380f5c2c0e1397f29528fae7713ffa3b39947
spiismnz <sp.iism@gmail.com>
2021-11-17 00:25:59 +1300

    show/hide signin/signout links conditionally

--- front-end-react/src/auth/index.js
+++ front-end-react/src/auth/index.js
@@ -57,3 +57,15 @@ export const signout = next => {
             .catch(err => console.log(err));
     }
 };
+
+
+export const isAuthenticated = () => {
+    if (typeof window == 'undefined') {
+        return false;
+    }
+    if (localStorage.getItem('jwt')) {
+        return JSON.parse(localStorage.getItem('jwt'));
+    } else {
+        return false;
+    }
+};
--- front-end-react/src/core/Menu.js
+++ front-end-react/src/core/Menu.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import {NavLink} from 'react-router-dom';
-import {signout} from '../auth';
+import {signout, isAuthenticated} from '../auth';
 
 
 
@@ -15,23 +15,32 @@ const Menu = () => (
                 <NavLink className="nav-link" to="/">Home</NavLink>
             </li>
 
-            <li className="nav-item">
-                <NavLink className="nav-link" to="/signin">Signin</NavLink>
-            </li>
+            {!isAuthenticated() && (
+                <Fragment>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="/signin">Signin</NavLink>
+                    </li>
 
-            <li className="nav-item">
-                <NavLink className="nav-link" to="/signup">Signup</NavLink>
-            </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="/signup">Signup</NavLink>
+                    </li>
+                </Fragment>
+            )}
 
-            <li className="nav-item">
-                <span className="nav-link" 
-                style={{cursor:'pointer', color:'black' }} 
-                onClick={() => signout(() => {
-                    //back to home after sign out (might not a good solution yet, cannot deal with react-router-dom for now)
-                    window.location.href = '/';
-                })}>
-                    Signout</span>
-            </li>
+            {isAuthenticated() && (
+                    <li className="nav-item">
+                        <span   className="nav-link" 
+                                style={{cursor:'pointer', color:'black' }} 
+                                onClick={() => signout(() => {
+                                    //back to home after sign out (might not a good solution yet, cannot deal with react-router-dom for now)
+                                    window.location.href = '/';
+                                })}>
+                        Signout</span>
+                    </li>
+            )}
+
+
+
         </ul>
     </div>
 );

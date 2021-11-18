c632fe20529ecfe132b2bc3506c93405feab0781
spiismnz <sp.iism@gmail.com>
2021-11-19 01:35:48 +1300

    create product (cannot add photo -check backend)

--- back-end-nodejs/controllers/product.js
+++ back-end-nodejs/controllers/product.js
@@ -1,9 +1,27 @@
-const Product = require('../models/product');
 const formidable = require('formidable');
 const _ = require('lodash');
 const fs = require('fs');
+const Product = require('../models/product');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
+exports.productById = (req, res, next, id) => {
+    Product.findById(id)
+        .populate('category')
+        .exec((err, product) => {
+            if (err || !product) {
+                return res.status(400).json({
+                    error: 'Product not found'
+                });
+            }
+            req.product = product;
+            next();
+        });
+};
+
+exports.read = (req, res) => {
+    req.product.photo = undefined;
+    return res.json(req.product);
+};
 
 exports.create = (req, res) => {
     let form = new formidable.IncomingForm();
@@ -29,7 +47,7 @@ exports.create = (req, res) => {
         // 1mb = 1000000
 
         if (files.photo) {
-            //console.log("FILES PHOTO: ", files.photo);
+            // console.log("FILES PHOTO: ", files.photo);
             if (files.photo.size > 1000000) {
                 return res.status(400).json({
                     error: 'Image should be less than 1mb in size'
@@ -51,30 +69,6 @@ exports.create = (req, res) => {
     });
 };
 
-
-
-
-exports.productById = (req, res, next, id) => {
-    Product.findById(id)
-        .populate('category')
-        .exec((err, product) => {
-            if (err || !product) {
-                return res.status(400).json({
-                    error: 'Product not found'
-                });
-            }
-            req.product = product;
-            next();
-        });
-};
-
-
-exports.read = (req, res) => {
-    req.product.photo = undefined;
-    return res.json(req.product);
-};
-
-
 exports.remove = (req, res) => {
     let product = req.product;
     product.remove((err, deletedProduct) => {
@@ -84,13 +78,10 @@ exports.remove = (req, res) => {
             });
         }
         res.json({
-            //deletedProduct,
             message: 'Product deleted successfully'
         });
     });
 };
-
-
 
 exports.update = (req, res) => {
     let form = new formidable.IncomingForm();
@@ -99,15 +90,6 @@ exports.update = (req, res) => {
         if (err) {
             return res.status(400).json({
                 error: 'Image could not be uploaded'
-            });
-        }
-
-        // check for all fields
-        const { name, description, price, category, quantity, shipping } = fields;
-
-        if (!name || !description || !price || !category || !quantity || !shipping) {
-            return res.status(400).json({
-                error: 'All fields are required'
             });
         }
 
@@ -139,8 +121,6 @@ exports.update = (req, res) => {
     });
 };
 
-
-
 /**
  * sell / arrival
  * by sell = /products?sortBy=sold&order=desc&limit=4
@@ -148,8 +128,7 @@ exports.update = (req, res) => {
  * if no params are sent, then all products are returned
  */
 
-
- exports.list = (req, res) => {
+exports.list = (req, res) => {
     let order = req.query.order ? req.query.order : 'asc';
     let sortBy = req.query.sortBy ? req.query.sortBy : '_id';
     let limit = req.query.limit ? parseInt(req.query.limit) : 6;
@@ -169,13 +148,12 @@ exports.update = (req, res) => {
         });
 };
 
-
 /**
  * it will find the products based on the req product category
  * other products that has the same category, will be returned
  */
 
- exports.listRelated = (req, res) => {
+exports.listRelated = (req, res) => {
     let limit = req.query.limit ? parseInt(req.query.limit) : 6;
 
     Product.find({ _id: { $ne: req.product }, category: req.product.category })
@@ -202,7 +180,6 @@ exports.listCategories = (req, res) => {
     });
 };
 
-
 /**
  * list products by search
  * we will implement product search in react frontend
@@ -210,23 +187,20 @@ exports.listCategories = (req, res) => {
  * as the user clicks on those checkbox and radio buttons
  * we will make api request and show the products to users based on what he wants
  */
- 
-// route - make sure its post
-//router.post("/products/by/search", listBySearch);
- 
+
 exports.listBySearch = (req, res) => {
-    let order = req.body.order ? req.body.order : "desc";
-    let sortBy = req.body.sortBy ? req.body.sortBy : "_id";
+    let order = req.body.order ? req.body.order : 'desc';
+    let sortBy = req.body.sortBy ? req.body.sortBy : '_id';
     let limit = req.body.limit ? parseInt(req.body.limit) : 100;
     let skip = parseInt(req.body.skip);
     let findArgs = {};
- 
+
     // console.log(order, sortBy, limit, skip, req.body.filters);
     // console.log("findArgs", findArgs);
- 
+
     for (let key in req.body.filters) {
         if (req.body.filters[key].length > 0) {
-            if (key === "price") {
+            if (key === 'price') {
                 // gte -  greater than price [0-10]
                 // lte - less than
                 findArgs[key] = {
@@ -238,17 +212,17 @@ exports.listBySearch = (req, res) => {
             }
         }
     }
- 
+
     Product.find(findArgs)
-        .select("-photo")
-        .populate("category")
+        .select('-photo')
+        .populate('category')
         .sort([[sortBy, order]])
         .skip(skip)
         .limit(limit)
         .exec((err, data) => {
             if (err) {
                 return res.status(400).json({
-                    error: "Products not found"
+                    error: 'Products not found'
                 });
             }
             res.json({
@@ -264,4 +238,47 @@ exports.photo = (req, res, next) => {
         return res.send(req.product.photo.data);
     }
     next();
+};
+
+exports.listSearch = (req, res) => {
+    // create query object to hold search value and category value
+    const query = {};
+    // assign search value to query.name
+    if (req.query.search) {
+        query.name = { $regex: req.query.search, $options: 'i' };
+        // assigne category value to query.category
+        if (req.query.category && req.query.category != 'All') {
+            query.category = req.query.category;
+        }
+        // find the product based on query object with 2 properties
+        // search and category
+        Product.find(query, (err, products) => {
+            if (err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                });
+            }
+            res.json(products);
+        }).select('-photo');
+    }
+};
+
+exports.decreaseQuantity = (req, res, next) => {
+    let bulkOps = req.body.order.products.map(item => {
+        return {
+            updateOne: {
+                filter: { _id: item._id },
+                update: { $inc: { quantity: -item.count, sold: +item.count } }
+            }
+        };
+    });
+
+    Product.bulkWrite(bulkOps, {}, (error, products) => {
+        if (error) {
+            return res.status(400).json({
+                error: 'Could not update product'
+            });
+        }
+        next();
+    });
 };
--- front-end-react/src/admin/apiAdmin.js
+++ front-end-react/src/admin/apiAdmin.js
@@ -17,3 +17,43 @@ export const createCategory = (userId, token, category) => {
             console.log(err);
         });
 };
+
+
+export const createProduct = (userId, token, product) => {
+    return fetch(`${API}/product/create/${userId}`, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${token}`
+        },
+        body: product
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+
+export const getCategory = categoryId => {
+    return fetch(`${API}/category/${categoryId}`, {
+        method: 'GET'
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+
+export const getCategories = () => {
+    return fetch(`${API}/categories`, {
+        method: 'GET'
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};

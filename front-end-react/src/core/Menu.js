89a72c6d7a1b68e17d14132e40985dc0d110bcdb
spiism <sp.iism@gmail.com>
2021-12-19 17:49:05 +1300

    cart page on Menu

--- front-end-react/src/core/Menu.js
+++ front-end-react/src/core/Menu.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 import { Link, withRouter } from "react-router-dom";
 import { signout, isAuthenticated } from "../auth";
+import {itemTotal} from "./cartHelpers";
 
 const isActive = (history, path) => {
   if (history.location.pathname === path) {
@@ -22,6 +23,12 @@ const Menu = ({ history }) => (
       <li className="nav-item">
         <Link className="nav-link" style={isActive(history, "/shop")} to="/shop">
           Shop
+        </Link>
+      </li>
+
+      <li className="nav-item">
+        <Link className="nav-link" style={isActive(history, "/cart")} to="/cart">
+          Cart <sup><small className="cart-badge">{itemTotal()}</small></sup>
         </Link>
       </li>
 
--- front-end-react/src/core/cartHelpers.js
+++ front-end-react/src/core/cartHelpers.js
@@ -26,3 +26,63 @@ export const addItem = (item = [], count = 0, next = f => f) => {
         next();
     }
 };
+
+export const itemTotal = () => {
+    if (typeof window !== 'undefined') {
+        if (localStorage.getItem('cart')) {
+            return JSON.parse(localStorage.getItem('cart')).length;
+        }
+    }
+    return 0;
+};
+
+export const getCart = () => {
+    if (typeof window !== 'undefined') {
+        if (localStorage.getItem('cart')) {
+            return JSON.parse(localStorage.getItem('cart'));
+        }
+    }
+    return [];
+};
+
+export const updateItem = (productId, count) => {
+    let cart = [];
+    if (typeof window !== 'undefined') {
+        if (localStorage.getItem('cart')) {
+            cart = JSON.parse(localStorage.getItem('cart'));
+        }
+
+        cart.map((product, i) => {
+            if (product._id === productId) {
+                cart[i].count = count;
+            }
+        });
+
+        localStorage.setItem('cart', JSON.stringify(cart));
+    }
+};
+
+export const removeItem = productId => {
+    let cart = [];
+    if (typeof window !== 'undefined') {
+        if (localStorage.getItem('cart')) {
+            cart = JSON.parse(localStorage.getItem('cart'));
+        }
+
+        cart.map((product, i) => {
+            if (product._id === productId) {
+                cart.splice(i, 1);
+            }
+        });
+
+        localStorage.setItem('cart', JSON.stringify(cart));
+    }
+    return cart;
+};
+
+export const emptyCart = next => {
+    if (typeof window !== 'undefined') {
+        localStorage.removeItem('cart');
+        next();
+    }
+};

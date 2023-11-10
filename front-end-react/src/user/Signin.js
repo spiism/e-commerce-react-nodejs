1c31b53d01c58f17ad9f102b816608477ff08d7d
spiism <sp.iism@gmail.com>
2023-11-11 05:32:51 +1300

    remove temp password for testing

--- front-end-react/src/user/Signin.js
+++ front-end-react/src/user/Signin.js
@@ -5,8 +5,8 @@ import { signin, authenticate, isAuthenticated } from "../auth";
 
 const Signin = () => {
     const [values, setValues] = useState({
-        email: "kate@gmail.com",
-        password: "123456",
+        email: "",
+        password: "",
         error: "",
         loading: false,
         redirectToReferrer: false
